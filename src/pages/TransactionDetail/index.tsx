111df7f321aa1aa1d1af4af7625980d3c73a397f
HarshaGunathilake <harshagunathilaka5@gmail.com>
2024-07-30 20:28:51 +0530

    transaction details

--- src/pages/TransactionDetail/index.tsx
+++ src/pages/TransactionDetail/index.tsx
@@ -10,17 +10,19 @@ import _ from "lodash";
 import { useNavigate } from "react-router-dom";
 
 function Main() {
-
   const navigate = useNavigate();
-  
-  const gotoDetails = ()=>{
+
+  const gotoDetails = () => {
     navigate("/transactions");
-  }
+  };
   return (
     <div className="grid grid-cols-12 gap-y-10 gap-x-6">
       <div className="col-span-12">
         <div className="flex flex-col lg:h-10 gap-y-3 lg:items-center lg:flex-row cursor-pointer">
-          <div className="text-lg font-medium group-[.mode--light]:text-white flex items-center" onClick={gotoDetails}>
+          <div
+            className="text-lg font-medium group-[.mode--light]:text-white flex items-center"
+            onClick={gotoDetails}
+          >
             Orders
             <Lucide
               icon="ArrowRight"
@@ -115,7 +117,7 @@ function Main() {
                 </div>
                 <div className="border rounded-[0.6rem] border-slate-200/80 dark:border-darkmode-400 relative mt-3">
                   <div className="absolute left-0 px-3 ml-4 -mt-2 text-xs uppercase bg-white text-slate-500">
-                    <div className="-mt-px">Shipping Details</div>
+                    <div className="-mt-px">Other Details</div>
                   </div>
                   <div className="p-5 mt-2.5 flex flex-col gap-5">
                     <div className="flex items-center">
@@ -124,10 +126,8 @@ function Main() {
                         className="w-4 h-4 mr-2.5 stroke-[1.3] text-slate-500"
                       />
                       <div className="flex flex-col flex-wrap w-full sm:items-center sm:flex-row gap-y-1">
-                        <div className="sm:mr-auto w-54">
-                          Total Price ({_.random(2, 20)} items):
-                        </div>
-                        ${transactions.fakeTransactions()[0].amount}
+                        <div className="sm:mr-auto w-54">Order Number :</div>
+                        86e6a51b
                       </div>
                     </div>
                     <div className="flex items-center">
@@ -136,10 +136,8 @@ function Main() {
                         className="w-4 h-4 mr-2.5 stroke-[1.3] text-slate-500"
                       />
                       <div className="flex flex-col flex-wrap w-full sm:items-center sm:flex-row gap-y-1">
-                        <div className="sm:mr-auto w-54">
-                          Total Shipping Cost:
-                        </div>
-                        ${_.random(30, 100)}
+                        <div className="sm:mr-auto w-54">Ticket ID</div>
+                        bb0f9554
                       </div>
                     </div>
                     <div className="flex items-center">
@@ -148,109 +146,12 @@ function Main() {
                         className="w-4 h-4 mr-2.5 stroke-[1.3] text-slate-500"
                       />
                       <div className="flex flex-col flex-wrap w-full sm:items-center sm:flex-row gap-y-1">
-                        <div className="sm:mr-auto w-54">Grand Total:</div>$
+                        <div className="sm:mr-auto w-54">Amount:</div>$
                         {transactions.fakeTransactions()[0].amount}
                       </div>
                     </div>
                   </div>
                 </div>
-                {/* <div className="border rounded-[0.6rem] border-slate-200/80 dark:border-darkmode-400 relative mt-3">
-                  <div className="absolute left-0 px-3 ml-4 -mt-2 text-xs uppercase bg-white text-slate-500">
-                    <div className="-mt-px">Buyer Details</div>
-                  </div>
-                  <div className="p-5 mt-2.5 flex flex-col gap-5">
-                    <div className="flex items-center">
-                      <Lucide
-                        icon="Clipboard"
-                        className="w-4 h-4 mr-2.5 stroke-[1.3] text-slate-500"
-                      />
-                      <div className="flex flex-col flex-wrap w-full sm:items-center sm:flex-row gap-y-1">
-                        <div className="sm:mr-auto w-54">Name:</div>
-                        <a
-                          href=""
-                          className="underline decoration-dotted decoration-primary/30 underline-offset-[3px]"
-                        >
-                          {transactions.fakeTransactions()[0].user.name}
-                        </a>
-                      </div>
-                    </div>
-                    <div className="flex items-center">
-                      <Lucide
-                        icon="Calendar"
-                        className="w-4 h-4 mr-2.5 stroke-[1.3] text-slate-500"
-                      />
-                      <div className="flex flex-col flex-wrap w-full sm:items-center sm:flex-row gap-y-1">
-                        <div className="sm:mr-auto w-54">Phone Number:</div>
-                        {transactions.fakeTransactions()[0].user.phone}
-                      </div>
-                    </div>
-                    <div className="flex items-center">
-                      <Lucide
-                        icon="Clock"
-                        className="w-4 h-4 mr-2.5 stroke-[1.3] text-slate-500"
-                      />
-                      <div className="flex flex-col flex-wrap w-full sm:items-center sm:flex-row gap-y-1">
-                        <div className="sm:mr-auto w-54">Address:</div>
-                        <a
-                          href=""
-                          className="flex items-center underline decoration-dotted decoration-primary/30 underline-offset-[3px]"
-                        >
-                          <Lucide
-                            icon="MapPin"
-                            className="w-3.5 h-3.5 mr-1.5"
-                          />
-                          View Address
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div> */}
-                {/* <div className="border rounded-[0.6rem] border-slate-200/80 dark:border-darkmode-400 relative mt-3">
-                  <div className="absolute left-0 px-3 ml-4 -mt-2 text-xs uppercase bg-white text-slate-500">
-                    <div className="-mt-px">Shipping Information</div>
-                  </div>
-                  <div className="p-5 mt-2.5 flex flex-col gap-5">
-                    <div className="flex items-center">
-                      <Lucide
-                        icon="Clipboard"
-                        className="w-4 h-4 mr-2.5 stroke-[1.3] text-slate-500"
-                      />
-                      <div className="flex flex-col flex-wrap w-full sm:items-center sm:flex-row gap-y-1">
-                        <div className="sm:mr-auto w-54">Courier:</div>
-                        Left4code Express
-                      </div>
-                    </div>
-                    <div className="flex items-center">
-                      <Lucide
-                        icon="Calendar"
-                        className="w-4 h-4 mr-2.5 stroke-[1.3] text-slate-500"
-                      />
-                      <div className="flex flex-col flex-wrap w-full sm:items-center sm:flex-row gap-y-1">
-                        <div className="sm:mr-auto w-54">Tracking Number:</div>
-                        {_.random(1000000, 5000000)}
-                      </div>
-                    </div>
-                    <div className="flex items-center">
-                      <Lucide
-                        icon="Clock"
-                        className="w-4 h-4 mr-2.5 stroke-[1.3] text-slate-500"
-                      />
-                      <div className="flex flex-col flex-wrap w-full sm:items-center sm:flex-row gap-y-1">
-                        <div className="sm:mr-auto w-54">Address:</div>
-                        <a
-                          href=""
-                          className="flex items-center underline decoration-dotted decoration-primary/30 underline-offset-[3px]"
-                        >
-                          <Lucide
-                            icon="MapPin"
-                            className="w-3.5 h-3.5 mr-1.5"
-                          />
-                          View Address
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div> */}
               </div>
             </div>
           </div>
@@ -270,16 +171,6 @@ function Main() {
                               Item
                             </div>
                           </Table.Td>
-                          {/* <Table.Td className="px-0 py-0 [&_div]:first:border-l [&_div]:last:border-r [&_div]:first:rounded-l-md [&_div]:last:rounded-r-md border-b-0">
-                            <div className="px-5 py-4 font-medium text-right bg-slate-50 border-slate-200/80 text-slate-500 border-y">
-                              Quantity
-                            </div>
-                          </Table.Td> */}
-                          {/* <Table.Td className="px-0 py-0 [&_div]:first:border-l [&_div]:last:border-r [&_div]:first:rounded-l-md [&_div]:last:rounded-r-md border-b-0">
-                            <div className="px-5 py-4 font-medium text-right bg-slate-50 border-slate-200/80 text-slate-500 border-y">
-                              Rate
-                            </div>
-                          </Table.Td> */}
                           <Table.Td className="px-0 py-0 [&_div]:first:border-l [&_div]:last:border-r [&_div]:first:rounded-l-md [&_div]:last:rounded-r-md border-b-0">
                             <div className="px-5 py-4 font-medium text-right bg-slate-50 border-slate-200/80 text-slate-500 border-y">
                               Amount
@@ -305,16 +196,6 @@ function Main() {
                                   <div className="ml-5">{faker.orderId}</div>
                                 </div>
                               </Table.Td>
-                              {/* <Table.Td className="py-4 text-right border-dashed dark:bg-darkmode-600">
-                              <div className="whitespace-nowrap">
-                                {_.random(2, 5)}
-                              </div>
-                            </Table.Td> */}
-                              {/* <Table.Td className="py-4 text-right border-dashed dark:bg-darkmode-600">
-                              <div className="whitespace-nowrap">
-                                $142.00
-                              </div>
-                            </Table.Td> */}
                               <Table.Td className="py-4 text-right border-dashed dark:bg-darkmode-600">
                                 <div className="font-medium whitespace-nowrap">
                                   $142.43
@@ -326,42 +207,10 @@ function Main() {
                       </Table.Tbody>
                     </Table>
                   </div>
-                  {/* <div className="flex flex-col gap-3.5 pr-5 mt-3 mb-5 ml-auto text-right">
-                    <div className="flex items-center justify-end">
-                      <div className="text-slate-500">Subtotal:</div>
-                      <div className="w-20 font-medium sm:w-48 text-slate-600">
-                        ${transactions.fakeTransactions()[0].amount}
-                      </div>
-                    </div>
-                    <div className="flex items-center justify-end">
-                      <div className="text-slate-500">Total:</div>
-                      <div className="w-20 font-medium sm:w-48 text-slate-600">
-                        ${transactions.fakeTransactions()[0].amount}
-                      </div>
-                    </div>
-                    <div className="flex items-center justify-end">
-                      <div className="text-slate-500">Tax:</div>
-                      <div className="w-20 font-medium sm:w-48 text-slate-600">
-                        ${_.random(30, 100)}
-                      </div>
-                    </div>
-                    <div className="flex items-center justify-end">
-                      <div className="text-slate-500">Amount paid:</div>
-                      <div className="w-20 font-medium sm:w-48 text-slate-600">
-                        ${transactions.fakeTransactions()[0].amount}
-                      </div>
-                    </div>
-                    <div className="flex items-center justify-end">
-                      <div className="text-slate-500">Due balance:</div>
-                      <div className="w-20 font-medium sm:w-48 text-slate-600">
-                        ${_.random(30, 100)}
-                      </div>
-                    </div>
-                  </div> */}
                 </div>
               </div>
             </div>
-            <div className="flex flex-col p-5 box box--stacked">
+            <div className="flex flex-col p-5 box">
               <div className="border rounded-[0.6rem] border-slate-200/80 dark:border-darkmode-400 relative mt-3">
                 <div className="absolute left-0 px-3 ml-4 -mt-2 text-xs uppercase bg-white text-slate-500">
                   <div className="-mt-px">Event Info</div>
@@ -369,14 +218,18 @@ function Main() {
                 <div className="px-5 py-2 mt-4 flex flex-col gap-3.5">
                   <div className="mb-3 last:mb-0 relative">
                     <a href="" className="font-medium text-primary">
-                      Transaction Completed.
+                      BahrainFORMULA 1 GULF AIR BAHRAIN GR...
                     </a>
                     <div className="flex flex-col sm:flex-row sm:items-center gap-y-1.5 mt-1.5 leading-relaxed text-slate-500 text-[0.8rem]">
-                      Funds will be forwarded to the seller.
+                      Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry. Lorem Ipsum has been the industry's
+                      standard dummy text ever since the 1500s, when an unknown
+                      printer took a galley of type and scrambled it to make a
+                      type specimen book.
                     </div>
                     <div className="sm:w-1/2 my-3.5 p-1 border rounded-[0.6rem] bg-slate-50/80">
                       <div className="grid grid-cols-1 md:grid-cols-3 overflow-hidden rounded-[0.6rem]">
-                        <div className="h-20 overflow-hidden border image-fit border-slate-100 saturate-[.6] hover:saturate-100 cursor-pointer">
+                        <div className="h-40 overflow-hidden border image-fit border-slate-100 saturate-[.6] hover:saturate-100 cursor-pointer">
                           <ImageZoom
                             alt="Tailwise - Admin Dashboard Template"
                             src={
@@ -388,32 +241,9 @@ function Main() {
                       </div>
                     </div>
                     <div className="mt-1.5 text-xs text-slate-500">
-                      25 Mar 2046, 10:28 AM
+                      Event Date: 25 Mar 2046, 10:28 AM
                     </div>
                   </div>
-                  <div className="mb-3 last:mb-0 relative">
-                    <a href="" className="font-medium text-primary">
-                      The order has arrived.
-                    </a>
-                    <div className="flex flex-col sm:flex-row sm:items-center gap-y-1.5 mt-1.5 leading-relaxed text-slate-500 text-[0.8rem]">
-                      Received by Calvin.
-                    </div>
-                    <div className="mt-1.5 text-xs text-slate-500">
-                      23 Mar 2023, 08:28 AM
-                    </div>
-                  </div>
-                  <div className="mb-3 last:mb-0 relative">
-                    <a href="" className="font-medium text-primary">
-                      The order has been sent.
-                    </a>
-                    <div className="flex flex-col sm:flex-row sm:items-center gap-y-1.5 mt-1.5 leading-relaxed text-slate-500 text-[0.8rem]">
-                      The order is being shipped by courier.
-                    </div>
-                    <div className="mt-1.5 text-xs text-slate-500">
-                      23 Mar 2023, 12:21 AM
-                    </div>
-                  </div>
-                 
                 </div>
               </div>
             </div>
